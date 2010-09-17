2f2ebaacafe96ddd420f06d37e2fcdbb383dfd8b
Hugo Duncan <hugo_duncan@yahoo.com>
2010-09-17 17:13:01 -0400

    change groupid to org.cloudhoist

--- project.clj
+++ project.clj
@@ -1,3 +1,3 @@
-(defproject pallet-lein "0.1.0-SNAPSHOT"
+(defproject org.cloudhoist/pallet-lein "0.1.0-SNAPSHOT"
   :description "Leiningen plugin for Pallet"
   :dev-dependencies [[org.clojure/clojure "1.2.0"]])
