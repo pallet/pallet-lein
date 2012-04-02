93d0ccc5265f299807ca2da82fb148da6b3fd43e
Hugo Duncan <hugo@hugoduncan.org>
2012-04-01 22:32:05 -0400

    Add sonatype to project.clj repositories

--- project.clj
+++ project.clj
@@ -2,4 +2,6 @@
   :description "Leiningen plugin for running Pallet tasks"
   :dev-dependencies [[org.clojure/clojure "1.2.1"]
                      [org.cloudhoist/pallet "0.6.6"]]
-  :eval-in-leiningen true)
+  :eval-in-leiningen true
+  :repositories
+  {"sonatype" "https://oss.sonatype.org/content/repositories/releases/"})
