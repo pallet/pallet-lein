39aed6f622e449f9edac4ce01417c4ff842526ab
David Santiago <david.santiago@gmail.com>
2012-04-02 23:04:03 -0700

    Update to run in lein2: Remove configleaf support, add pallet-lein.compat namespace with cross-version EIP, update pallet task code.

--- src/leiningen/pallet.clj
+++ src/leiningen/pallet.clj
@@ -1,8 +1,7 @@
 (ns leiningen.pallet
   "Pallet command line."
-  (:require
-   leiningen.compile
-   leiningen.test))
+  (:require [pallet-lein.compat :as compat]
+            leiningen.test))
 
 (defmacro maybe-shutdown-agents
   "Shutdown agents if required after body"
@@ -20,48 +19,44 @@
    For a list of tasks
      lein pallet help"
   ([project & args]
-     (if (and project (map? project))
-       (leiningen.compile/eval-in-project
-        project
-        `(do
-           (if-let [rv# (try
-                          (require '~'pallet.main)
-                          (catch java.io.FileNotFoundException e#
-                            (binding [*out* *err*]
-                              (println
-                               "Error loading pallet: " (.getMessage e#))
-                              (println
-                               "You need to have pallet as a project")
-                              (println
-                               "dependency or installed in ~/.lein/plugins"))
-                            1))]
-             rv#
-             (if-let [m# (ns-resolve
-                          (the-ns '~'pallet.main)
-                          '~'pallet-task)]
-               (try
-                 (let [env# (pallet-lein.configleaf/get-environment '~project)]
-                   (m# (concat ["-project-options" ~(pr-str project)] [~@args])
-                       :environment env#))
-                 (finally
-                  ~(when (and (bound? #'leiningen.test/*exit-after-tests*)
-                              leiningen.test/*exit-after-tests*)
-                     `(shutdown-agents))))
-               (do
-                 (binding [*out* *err*]
-                   (println "failed to resolve pallet.main/pallet-task"))
-                 1))))
-        nil
-        nil
-        '(require 'pallet-lein.configleaf))
-       (maybe-shutdown-agents
-        (require 'pallet.main)
-        ((ns-resolve (the-ns 'pallet.main) 'pallet-task) args)
-        (catch java.io.FileNotFoundException e
-          (println "Error loading pallet: " (.getMessage e))
-          (println "You need to install pallet and it's dependencies in")
-          (println "~/.lein/plugins in order to use the lein-pallet plugin")
-          (println "outside of a project.")
-          1))))
+     (let [project-str (pr-str project)
+           main-form
+           `(do
+              (if-let [rv# (try
+                             (require '~'pallet.main)
+                             (catch java.io.FileNotFoundException e#
+                               (binding [*out* *err*]
+                                 (println
+                                  "Error loading pallet: " (.getMessage e#))
+                                 (println
+                                  "You need to have pallet as a project")
+                                 (println
+                                  "dependency or installed in ~/.lein/plugins"))
+                               1))]
+                rv#
+                (if-let [m# (ns-resolve (the-ns '~'pallet.main) '~'pallet-task)]
+                  (try
+                    (let [env# (:pallet/environment (read-string ~project-str))]
+                      (m# (concat ["-project-options" ~(pr-str project)] [~@args])
+                          :environment env#))
+                    (finally
+                     ~(when (and (bound? #'leiningen.test/*exit-after-tests*)
+                                 leiningen.test/*exit-after-tests*)
+                        `(shutdown-agents))))
+                  (do
+                    (binding [*out* *err*]
+                      (println "failed to resolve pallet.main/pallet-task"))
+                    1))))]
+       (if (and project (map? project))
+         (compat/eval-in-project project main-form nil)
+         (maybe-shutdown-agents
+          (require 'pallet.main)
+          ((ns-resolve (the-ns 'pallet.main) 'pallet-task) args)
+          (catch java.io.FileNotFoundException e
+            (println "Error loading pallet: " (.getMessage e))
+            (println "You need to install pallet and it's dependencies in")
+            (println "~/.lein/plugins in order to use the lein-pallet plugin")
+            (println "outside of a project.")
+            1)))))
   ([arg] (pallet nil arg))
   ([] (pallet nil)))
