0f084becf0275a41bebe14fe5627b17664ce5f08
David Santiago <david.santiago@gmail.com>
2012-04-03 00:37:30 -0700

    Add pallet to dev profile, for lein2.

--- project.clj
+++ project.clj
@@ -2,6 +2,7 @@
   :description "Leiningen plugin for running Pallet tasks"
   :dev-dependencies [[org.clojure/clojure "1.2.1"]
                      [org.cloudhoist/pallet "0.6.6"]]
+  :profiles {:dev {:dependencies [[org.cloudhoist/pallet "0.6.6"]]}}
   :eval-in-leiningen true
   :repositories
   {"sonatype" "https://oss.sonatype.org/content/repositories/releases/"})
