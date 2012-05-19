930a0980543c19f61bf2e687aecf13089490cbdf
Hugo Duncan <hugo@hugoduncan.org>
2012-05-18 23:22:36 -0400

    Update dev dependencies to pallet 0.7.0

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
 (defproject org.cloudhoist/pallet-lein "0.5.0-SNAPSHOT"
   :description "Leiningen plugin for running Pallet tasks"
   :dev-dependencies [[org.clojure/clojure "1.2.1"]
-                     [org.cloudhoist/pallet "0.6.6"]]
-  :profiles {:dev {:dependencies [[org.cloudhoist/pallet "0.6.6"]]}}
+                     [org.cloudhoist/pallet "0.7.0"]]
+  :profiles {:dev {:dependencies [[org.cloudhoist/pallet "0.7.0"]]}}
   :eval-in-leiningen true
   :repositories
   {"sonatype" "https://oss.sonatype.org/content/repositories/releases/"})
