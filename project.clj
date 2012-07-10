bca2d34c79e070227a46167a9fc9503330e82369
Hugo Duncan <hugo@hugoduncan.org>
2012-07-10 13:25:02 -0400

    Prepare for next release cycle

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.cloudhoist/pallet-lein "0.5.0"
+(defproject org.cloudhoist/pallet-lein "0.5.1-SNAPSHOT"
   :description "Leiningen plugin for running Pallet tasks"
   :dev-dependencies [[org.clojure/clojure "1.2.1"]
                      [org.cloudhoist/pallet "0.7.0"]]
