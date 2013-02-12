9967175405b8c2077e0dd5f46c26bf031b56eac7
Hugo Duncan <hugo@hugoduncan.org>
2013-02-12 16:43:55 -0500

    Enforce xpcom or ws, and minimum clojure version

---
+++ src/pallet_lein/plugin.clj
@@ -0,0 +1,79 @@
+(ns pallet-lein.plugin
+  "Plugin to hook get-classpath, and ensure vbox xpcom and ws libs are
+exclusive"
+  (:require
+   [robert.hooke :refer [add-hook]]
+   [leiningen.core.classpath :refer [resolve-dependencies]]
+   [leiningen.core.main :refer [debug]]))
+
+(defn xpcom? [[artifact _]]
+  (= "vboxjxpcom" (name artifact)))
+
+(defn ws? [[artifact _]]
+  (= "vboxjws" (name artifact)))
+
+(defn clojure? [[artifact _]]
+  (and (= "org.clojure" (namespace artifact))
+       (= "clojure" (name artifact))))
+
+(def displace? #'leiningen.core.project/displace?)
+(def replace? #'leiningen.core.project/replace?)
+
+(defn- remove-xpcom-ws-conflicts
+  "Remove conflicts between virtualbox xpcom and ws libs.
+Assumes that these libs are only every present as direct dependencies."
+  [dependencies-key project]
+  (debug "Dependencies before cleanup"
+         dependencies-key (get project dependencies-key))
+  (let [p (update-in
+           project [dependencies-key]
+           (fn check-dependencies [deps]
+             (->
+              (reduce
+               (fn check-conflict [[deps seen] dependency]
+                 (if (or (xpcom? dependency) (ws? dependency))
+                   (if seen
+                     (cond
+                      (displace? seen) [(conj deps dependency) dependency]
+                      (replace? dependency) [(conj deps dependency) dependency]
+                      :else [deps seen])
+                     [(conj deps dependency) dependency])
+                   [(conj deps dependency) dependency]))
+               [[] nil]
+               deps)
+              first)))]
+    (debug "Dependencies after cleanup" (get p dependencies-key))
+    p))
+
+(defn- ensure-minimal-clojure-version
+  "Assure that a minimum version of clojure is in the dependencies"
+  [dependencies-key project]
+  (let [p (update-in
+           project [dependencies-key]
+           #(->>
+             %
+             (map
+              (fn [[artifact version :as dependency]]
+                (if (clojure? dependency)
+                  (let [[_ major minor]
+                        (re-matches #"([0-9]+)\.([0-9]+).*" version)]
+                    (if (and (= "1" major) (< (Integer/parseInt minor) 4))
+                      [artifact "1.4.0"]
+                      dependency))
+                  dependency)))
+             vec))]
+    (debug "Dependencies after minimal version check" (get p dependencies-key))
+    p))
+
+(defn remove-xpcom-ws-conflicts-wrapper
+  [f dependencies-key project & rest]
+  (f dependencies-key (remove-xpcom-ws-conflicts dependencies-key project)))
+
+(defn ensure-minimal-clojure-version-wrapper
+  [f dependencies-key project & rest]
+  (f dependencies-key
+     (ensure-minimal-clojure-version dependencies-key project)))
+
+(defn hooks []
+  (add-hook #'resolve-dependencies remove-xpcom-ws-conflicts-wrapper)
+  (add-hook #'resolve-dependencies ensure-minimal-clojure-version-wrapper))
