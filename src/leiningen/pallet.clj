6bab749e5898d7e2bbb0d82717a52996f6a2efb0
Hugo Duncan <hugo@hugoduncan.org>
2013-02-12 16:45:08 -0500

    Make plugin work without a project

--- src/leiningen/pallet.clj
+++ src/leiningen/pallet.clj
@@ -2,7 +2,7 @@
   "Pallet command line."
  (:require
   [leiningen.core.eval :refer [eval-in-project]]
-  [leiningen.core.project :refer [merge-profiles]]
+  [leiningen.core.project :refer [make merge-profiles]]
   leiningen.test))
 
 (defmacro maybe-shutdown-agents
@@ -20,16 +20,21 @@
   {:source-paths (:source-paths pallet ["pallet/src"])
    :resource-paths (:resource-paths pallet ["pallet/resources"])
    :dependencies '[^:displace [org.cloudhoist/pallet "0.8.0-SNAPSHOT"]
-                   ^:displace [org.cloudhoist/pallet-vmfest "0.3.0-SNAPSHOT"]
-                   ^:displace [vmfest "0.3.0-SNAPSHOT"
-                               :exclusions [commons-logging]]
+                   ^:displace [org.cloudhoist/pallet-vmfest "0.3.0-alpha.1"]
                    ^:displace [org.clojars.tbatchelli/vboxjxpcom "4.2.4"]
                    ;; [org.clojars.tbatchelli/vboxjws "4.2.4"]
+
+                   ;; Add an explicit version of clojure.  A project
+                   ;; can override this, but the hooks ensure a minimal
+                   ;; version
+                   ^:displace [org.clojure/clojure "1.4.0"]
 
                    ;; we do this to get a logging configuration
                    ;; this needs some thinking about
                    ^:displace [org.cloudhoist/pallet-lein "0.6.0-SNAPSHOT"]
                    ^:displace [ch.qos.logback/logback-classic "1.0.9"]]
+   :jvm-opts ["-XX:TieredStopAtLevel=1"
+              "-XX:+TieredCompilation"]
    :repositories
    {"sonatype"
     {:url "https://oss.sonatype.org/content/repositories/releases/"
@@ -44,65 +49,57 @@
   pallet
   "Launch pallet tasks from the command line.
    `lein pallet help` for more details."
-  ([project & args]
-     (let [project (merge-profiles
-                    project
-                    (or
-                     (seq (filter (partial has-profile? pallet) [:pallet]))
-                     [(pallet-profile project)]))
-           [project args] (if (and (map? project)
-                                   (every?
-                                    identity
-                                    (map project [:name :group :version])))
-                            [project args]
-                            [nil [project]])
-           project-str (pr-str (dissoc project :repositories))
-           main-form
-           `(do
-              (if-let [rv# (try
-                             (require '~'pallet.main)
-                             (catch java.io.FileNotFoundException e#
-                               (binding [*out* *err*]
-                                 (println
-                                  "Error loading pallet: " (.getMessage e#))
-                                 (println
-                                  "You need to have pallet as a project")
-                                 (println
-                                  "dependency or installed in ~/.lein/plugins"))
-                               1))]
-                ;; If there was an error, then the return value is 1 and that is
-                ;; returned.
-                rv#
-                ;; If the require worked, the return value was nil and the else
-                ;; clause happens.
-                (if-let [m# (ns-resolve (the-ns '~'pallet.main) '~'pallet-task)]
-                  (try
-                    (let [env# (:pallet/environment (read-string ~project-str))
-                          s# (ns-resolve
-                              (the-ns '~'pallet.main) '~'add-service)]
-                      (when s#
-                        ;; create a default vmfest provider
-                        (s# :vbox {:provider :vmfest}))
-                      (m# (concat ["--project-options" ~project-str]
-                                  [~@args])
-                          :environment env#))
-                    (finally
-                      ~(when (and (bound? #'leiningen.test/*exit-after-tests*)
-                                  leiningen.test/*exit-after-tests*)
-                         `(shutdown-agents))))
-                  (do
-                    (binding [*out* *err*]
-                      (println
-                       "failed to resolve pallet.main/pallet-task"))))))]
-       (if (and project (map? project))
-         (eval-in-project project main-form nil)
-         (maybe-shutdown-agents
-          (require 'pallet.main)
-          ((ns-resolve (the-ns 'pallet.main) 'pallet-task) args)
-          (catch java.io.FileNotFoundException e
-            (println "Error loading pallet: " (.getMessage e))
-            (println "You need to install pallet and it's dependencies in")
-            (println "~/.lein/plugins in order to use the lein-pallet plugin")
-            (println "outside of a project.")
-            (throw (ex-info "Error loading pallet" {} e)))))))
-  ([] (pallet nil)))
+  [project & args]
+  (let [project (or project
+                    (make
+                     {:name "pallet-lein" :group "pallet" :version "0.1.0"}))
+        project (merge-profiles
+                 project
+                 (or
+                  (seq (filter (partial has-profile? pallet) [:pallet]))
+                  [(pallet-profile project)]))
+        [project args] (if (and (map? project)
+                                (every?
+                                 identity
+                                 (map project [:name :group :version])))
+                         [project args]
+                         [nil [project]])
+        project-str (pr-str (dissoc project :repositories))
+        main-form
+        `(do
+           (if-let [rv# (try
+                          (require '~'pallet.main)
+                          (catch java.io.FileNotFoundException e#
+                            (binding [*out* *err*]
+                              (println
+                               "Error loading pallet: " (.getMessage e#))
+                              (println
+                               "You need to have pallet as a project")
+                              (println
+                               "dependency or installed in ~/.lein/plugins"))
+                            1))]
+             ;; If there was an error, then the return value is 1 and that is
+             ;; returned.
+             rv#
+             ;; If the require worked, the return value was nil and the else
+             ;; clause happens.
+             (if-let [m# (ns-resolve (the-ns '~'pallet.main) '~'pallet-task)]
+               (try
+                 (let [env# (:pallet/environment (read-string ~project-str))
+                       s# (ns-resolve
+                           (the-ns '~'pallet.main) '~'add-service)]
+                   (when s#
+                     ;; create a default vmfest provider
+                     (s# :vbox {:provider :vmfest}))
+                   (m# (concat ["--project-options" ~project-str]
+                               [~@args])
+                       :environment env#))
+                 (finally
+                   ~(when (and (bound? #'leiningen.test/*exit-after-tests*)
+                               leiningen.test/*exit-after-tests*)
+                      `(shutdown-agents))))
+               (do
+                 (binding [*out* *err*]
+                   (println
+                    "failed to resolve pallet.main/pallet-task"))))))]
+    (eval-in-project project main-form nil)))
