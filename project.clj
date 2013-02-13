c61172279763c6f31cfc6bab9d411e8f5edd3436
Hugo Duncan <hugo@hugoduncan.org>
2013-02-13 18:53:32 -0500

    Update GroupId to com.palletops

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.cloudhoist/pallet-lein "0.6.0-SNAPSHOT"
+(defproject com.palletops/pallet-lein "0.6.0-SNAPSHOT"
   :description "Leiningen plugin for running Pallet tasks"
   :eval-in-leiningen true
   :min-lein-version "2.0.0"
