8e3c221aafbe4a9e4883bacac09c56bfef803d43
Hugo Duncan <hugo@hugoduncan.org>
2013-04-02 12:15:41 -0400

    Remove sonatype from project.clj

--- project.clj
+++ project.clj
@@ -5,6 +5,4 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :scm {:url "git@github.com:pallet/pallet-lein.git"}
   :eval-in-leiningen true
-  :min-lein-version "2.0.0"
-  :repositories
-  {"sonatype" "https://oss.sonatype.org/content/repositories/releases/"})
+  :min-lein-version "2.0.0")
