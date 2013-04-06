939de85684173f76de26505d41a4ad2eeb6eace3
Hugo Duncan <hugo@hugoduncan.org>
2013-04-06 00:36:45 -0400

    Updated version for next release cycle

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.palletops/pallet-lein "0.6.0-beta.9"
+(defproject com.palletops/pallet-lein "0.6.0-SNAPSHOT"
   :description "Leiningen plugin for running Pallet tasks"
   :url "http://palletops.com"
   :license {:name "Eclipse Public License"
