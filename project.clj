25c8ade0b6a460f6d9d11c0defe86674d153a773
Hugo Duncan <hugo@hugoduncan.org>
2013-06-06 16:24:25 -0400

    Remove classpath magic

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.palletops/pallet-lein "0.6.0-SNAPSHOT"
+(defproject com.palletops/pallet-lein "0.8.0-SNAPSHOT"
   :description "Leiningen plugin for running Pallet tasks"
   :url "http://palletops.com"
   :license {:name "Eclipse Public License"
